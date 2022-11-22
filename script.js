aa5d352186180634e733a3d63afd900be93b7687
mekoness <mekoness@gmail.com>
2022-11-21 21:25:44 -0800

    Ver 0 - first maths

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const outputTop = document.querySelector("#output-top");
 const outputMain = document.querySelector("#output-main");
-const acBtn = document.querySelector("#ac-btn")
-const cBtn = document.querySelector("#c-btn")
-const equalsBtn = document.querySelector("#equals-btn")
-const addBtn = document.querySelector("#add-btn")
+const acBtn = document.querySelector("#ac-btn");
+const cBtn = document.querySelector("#c-btn");
+const equalsBtn = document.querySelector("#equals-btn");
+const operatorBtns = document.querySelectorAll(".op");
 const numBtns = document.querySelectorAll(".num");
 
+let a = 0;
+let b = 0;
+let result = 0;
 let topValue = "";
 let mainValue = 0;
-let equation = "";
-let result = 0;
+let operator = "";
 outputTop.textContent = topValue;
 outputMain.textContent = mainValue;
 
 acBtn.addEventListener("click", () => {
     topValue = "";
     mainValue = 0;
+    result = 0;
     outputTop.textContent = topValue;
     outputMain.textContent = mainValue;
-})
+});
 
 cBtn.addEventListener("click", () => {
     topValue = topValue.slice(0, -1);
@@ -36,8 +39,64 @@ numBtns.forEach((btn) => {
     });
 });
 
-equalsBtn.addEventListener("click", evaluate)
+operatorBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        save(btn.textContent, btn);
+    });
+});
 
-function evaluate() {
+function save(op, btn) {
+    operator = op;
+    topValue += btn.innerText;
+    outputTop.textContent = topValue;
+    console.log(operator);
+    a = Number(mainValue);
+}
 
-};
+equalsBtn.addEventListener("click", () => evaluate(operator, a));
+
+function evaluate(operator, a) {
+    b = Number(mainValue);
+    switch (operator) {
+      case "+":
+        result = addition(a, b);
+        break;
+      case "−":
+        result = subtract(a, b);
+        break;
+      case "×":
+        result = multiply(a, b);
+        break;
+      case "÷":
+        if (b === 0) {
+            result = "ERROR";
+        }
+        else {
+            result = divide(a, b);
+        }
+        break;
+      default:
+        return;
+    };
+    topValue = "";
+    outputTop.textContent = topValue;
+    mainValue = result;
+    outputMain.textContent = mainValue;
+
+  };
+
+  function addition(a, b) {
+    return a + b;
+  }
+  
+function subtract(a, b) {
+    return a - b;
+  }
+  
+function multiply(a, b) {
+    return a * b;
+  }
+  
+function divide(a, b) {
+    return a / b;
+  }
