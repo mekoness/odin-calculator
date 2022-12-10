57e0de10a4125aece727d78ace651a11c00f891a
mekoness <mekoness@gmail.com>
2022-12-09 16:22:36 -0800

    Ver 1 working maths

--- script.js
+++ script.js
@@ -1,59 +1,42 @@
 const outputTop = document.querySelector("#output-top");
 const outputMain = document.querySelector("#output-main");
-const acBtn = document.querySelector("#ac-btn");
-const cBtn = document.querySelector("#c-btn");
-const equalsBtn = document.querySelector("#equals-btn");
-const operatorBtns = document.querySelectorAll(".op");
-const numBtns = document.querySelectorAll(".num");
 
 let a = 0;
 let b = 0;
 let result = 0;
 let topValue = "";
-let mainValue = 0;
+let mainValue = "";
 let operator = "";
-outputTop.textContent = topValue;
-outputMain.textContent = mainValue;
 
-acBtn.addEventListener("click", () => {
-    topValue = "";
-    mainValue = 0;
-    result = 0;
+document.addEventListener("click", (e) => {
+    if (e.target.id === "ac-btn") {
+        topValue = "";
+        mainValue = "";
+        result = 0;
+        render()
+    } else if (e.target.id === "equals-btn") {
+        evaluate(operator, a)
+    } else if (e.target.className === "btn op") {
+        save(e.target.textContent);
+    } else if (e.target.className === "btn num") {
+        mainValue += e.target.innerText;
+        render()
+    };
+});
+
+function render() {
     outputTop.textContent = topValue;
     outputMain.textContent = mainValue;
-});
-
-cBtn.addEventListener("click", () => {
-    topValue = topValue.slice(0, -1);
-    mainValue = 0;
-    outputTop.textContent = topValue;
-    outputMain.textContent = mainValue;
-});
-
-numBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        topValue += btn.innerText;
-        mainValue = btn.innerText;
-        outputTop.textContent = topValue;
-        outputMain.textContent = mainValue;
-    });
-});
-
-operatorBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        save(btn.textContent, btn);
-    });
-});
-
-function save(op, btn) {
-    operator = op;
-    topValue += btn.innerText;
-    outputTop.textContent = topValue;
-    console.log(operator);
-    a = Number(mainValue);
 }
 
-equalsBtn.addEventListener("click", () => evaluate(operator, a));
+render()
+
+function save(op) {
+    operator = op;
+    outputTop.textContent = topValue;
+    a = Number(mainValue);
+    mainValue = "";
+};
 
 function evaluate(operator, a) {
     b = Number(mainValue);
@@ -78,25 +61,23 @@ function evaluate(operator, a) {
       default:
         return;
     };
-    topValue = "";
-    outputTop.textContent = topValue;
+    topValue = result;
     mainValue = result;
-    outputMain.textContent = mainValue;
+    render()
+};
 
-  };
-
-  function addition(a, b) {
+function addition(a, b) {
     return a + b;
-  }
+};
   
 function subtract(a, b) {
     return a - b;
-  }
+};
   
 function multiply(a, b) {
     return a * b;
-  }
+};
   
 function divide(a, b) {
     return a / b;
-  }
+};
